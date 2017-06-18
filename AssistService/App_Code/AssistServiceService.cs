64101181ec76d0a410af3ea05f8eb4b090ef7c6d
JosephWanderer <banhstopjw@gmail.com>
2017-06-18 07:58:28 -0700

    Add Service and Client

---
+++ AssistService/App_Code/AssistServiceService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AssistServiceService" in code, svc and config file together.
+public class AssistServiceService : IAssistServiceService
+{
+
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public List<string> GetGrantTypes()
+    {
+        var granttypes = (from gt in db.GrantTypes
+                          select gt.GrantTypeName).ToList();
+        return granttypes;
+    }
+
+    public int Login(string email, string password)
+    {
+        int key = 0;
+        
+        
+        int result = db.usp_Login(email, password);
+
+        if(result != -1)
+        {
+            var userkey = (from k in db.People
+                           where k.PersonEmail.Equals(email)
+                           select k.PersonKey).FirstOrDefault();
+
+            key = (int)userkey;
+        }
+
+        return key;
+    }
+
+    public bool RegisterPerson(string lastname, string firstname, string email, string password, string aptnumber, string street, string city, string state, string zipcode, string homephone, string workphone)
+    {
+
+        bool result = true;
+
+        db.usp_Register(lastname, firstname, email, password, aptnumber, street, city, state, zipcode, homephone, workphone);
+
+        return result;
+    }
+
+    public int RequestGrant(int granttype, string requestexplanation, decimal amount, int userkey)
+    {
+        
+        int result = db.usp_AddRequest(granttype, requestexplanation, amount, userkey);
+        return result;
+    }
+
+    public List<GrantRequestInfo> ViewGrants(int userkey)
+    {
+        
+
+        var grantRequests = from rqs in db.GrantRequests
+                            where rqs.PersonKey == userkey
+                     select rqs;
+
+        List<GrantRequestInfo> requestsInfo = new List<GrantRequestInfo>();
+        foreach(var rq in grantRequests)
+        {
+                     
+
+                GrantRequestInfo grqst = new GrantRequestInfo();
+                grqst.GrantRequestAmount = (decimal)rq.GrantRequestAmount;
+                grqst.GrantRequestDate = rq.GrantRequestDate;
+                grqst.GrantRequestExplanation = rq.GrantRequestExplanation;
+             
+           
+                foreach (var review in rq.GrantReviews)
+                {
+                    grqst.GrantStatus = review.GrantRequestStatus;
+                    grqst.GrantAllocatedAmount = (decimal)review.GrantAllocationAmount;
+                    grqst.GrantReviewDate = review.GrantReviewDate.ToString() ;
+                }
+
+                if (grqst.GrantStatus == null)
+            {
+                grqst.GrantStatus = "Requires Review";
+                grqst.GrantAllocatedAmount = 0;
+                grqst.GrantReviewDate = "Pending";
+            }
+            
+
+
+
+            requestsInfo.Add(grqst);
+            
+        }
+
+        return requestsInfo;
+    }
+}
+
+
---
+++ CommunityAssistClient/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+</head>
+<body>
+    <form id="loginform" runat="server">
+    <div>
+        <h1>Login</h1>
+            <table>
+        <tr>
+            <td>Email</td>
+            <td>
+                <asp:TextBox ID="EmailTextBox" runat="server" TextMode="Email"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>
+                <asp:TextBox ID="PasswordTextBox" runat="server" TextMode="Password"></asp:TextBox>
+            </td>
+        </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="LoginButton" runat="server" Text="Log in" OnClick="LoginButton_Click" />
+                    </td>  
+                    <td><asp:Label ID="ResultLabel" runat="server" Text=""></asp:Label></td>
+                </tr>
+    </table>
+        <asp:LinkButton ID="RegistrationLinkButton" runat="server" PostBackUrl="~/Registration.aspx">Register</asp:LinkButton>
+        &nbsp;
+        <asp:LinkButton ID="ApplyLinkButton" runat="server" PostBackUrl="~/Apply.aspx">Apply for Grant</asp:LinkButton>
+        &nbsp;
+        <asp:LinkButton ID="GrantHistoryButton" runat="server" PostBackUrl="~/GrantHistory.aspx">View Your Grant History</asp:LinkButton>
+    </div>
+    </form>
+</body>
+</html>
